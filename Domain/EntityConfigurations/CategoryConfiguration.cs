fb19c69350eca640f94aa0d052f8d87677d98abd
Kenan Ölmezoglu <kennyolmez@gmail.com>
2022-10-25 22:09:26 +0200

    set up entity configurations

---
+++ Domain/EntityConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.EntityConfigurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ Domain/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.EntityConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            // Configure price property to be decimal with a number length of 18 and two decimal points.
+            builder.Property(p => p.Price)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
