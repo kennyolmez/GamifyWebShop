978e8f655038894b129cd100f3f1f1875a53d925
kenan <kennyolmez@gmail.com>
2022-10-30 16:19:50 +0100

    add CatalogServices and building Index controller

---
+++ ApplicationCore/Services/CatalogServices.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.DTOs;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class CatalogServices
+    {
+        private readonly ApplicationDbContext _context;
+        public CatalogServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<ProductDto>> GetAllProducts()
+        {
+            var products = await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.Brand)
+                .ToListAsync();
+
+            return products.Select(x => new ProductDto(x)).ToList();
+        }
+
+    }
+}
--- Web/Controllers/CatalogController.cs
+++ Web/Controllers/CatalogController.cs
@@ -1,21 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Services;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Reflection.Metadata.Ecma335;
 using Web.Models;
+using Web.ViewModels.CatalogViewModels;
 
 namespace Web.Controllers
 {
     public class CatalogController : Controller
     {
         private readonly ILogger<CatalogController> _logger;
+        private readonly CatalogServices _services;
 
-        public CatalogController(ILogger<CatalogController> logger)
+        public CatalogController(ILogger<CatalogController> logger, CatalogServices services)
         {
             _logger = logger;
+            _services = services;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? brandSelected, int? categorySelected, int? productTypeSelected)
         {
+            IndexViewModel viewModel = new IndexViewModel
+            {
+                // If whatever is to the left is null, use what's to the right.
+                BrandSelected = brandSelected ?? 0,
+                CategorySelected = categorySelected ?? 0,
+                ProductTypeSelected = productTypeSelected ?? 0,
+                Products = await _services.GetAllProducts(),
+            };
+
             return View();
         }
 
