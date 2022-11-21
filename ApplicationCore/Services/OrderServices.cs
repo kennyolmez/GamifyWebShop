bc537ab0f231b4047c6172a7bce7c5a0bb637342
kenan <kennyolmez@gmail.com>
2022-11-21 18:53:03 +0100

    add Order related services and controller for upcoming implementation of checkout function

---
+++ ApplicationCore/Services/OrderServices.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class OrderServices
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task CreateOrder()
+        {
+            // Fetch basket
+            // Create order entity 
+            // Save changes
+            // Delete basket
+        }
+    }
+}
---
+++ Web/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly OrderServices _orderServices;
+        public OrderController(OrderServices orderServices, ILogger<OrderController> logger)
+        {
+            _orderServices = orderServices;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // Fetch basket/products
+            // Use items to model viewmodel
+            // Return to view
+            return View();
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            // Fetch basket
+            // Execute CreateOrder method from OrderServices
+            return View();
+        }
+    }
+}
