8ad21a4415327c6f2945a9f18022936955badbdc
kenan <kennyolmez@gmail.com>
2022-11-30 19:12:06 +0100

    add migration and update database to reflect entity changes

---
+++ Infrastructure/Migrations/20221129204233_Added Picture URL to CartItem.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddedPictureURLtoCartItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "ShoppingCartItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "ShoppingCartItems");
+        }
+    }
+}
