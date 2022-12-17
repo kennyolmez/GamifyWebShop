890c5238934fd9fbad358204e7845c7198be423d
kenan <kennyolmez@gmail.com>
2022-12-17 17:54:56 +0100

    update UI

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+function decrementValue(index) {
+    var input = document.getElementById("numberInput-" + index);
+    var value = parseFloat(input.value);
+    if (!isNaN(value) && value > 0) {
+        input.value = value - 1;
+    }
+}
+
+function incrementValue(index) {
+    var input = document.getElementById("numberInput-" + index);
+    var value = parseFloat(input.value);
+    if (!isNaN(value)) {
+        input.value = value + 1;
+    }
+}
+
+
+
+function decrementRating() {
+    var input = document.getElementById("ratingInput");
+    var value = parseInt(input.value);
+    if (value > 0) {
+        input.value = value - 1;
+    }
+}
+
+function incrementRating() {
+    var input = document.getElementById("ratingInput");
+    var value = parseInt(input.value);
+    if (value < 5) {
+        input.value = value + 1;
+    }
+}
